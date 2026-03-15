3e932cf5e4b58b4f3a0015b2d3c5d3a63312c10d
Salvatore Di Genua <salvatore.digenua@orbyta.it>
2026-03-15 13:03:36 +0100

    add gap

--- src/app/features/profile/event-history/event-history.component.ts
+++ src/app/features/profile/event-history/event-history.component.ts
@@ -86,7 +86,7 @@ import { EventsService } from '../../../core/services/events.service';
               </div>
 
               <div class="flex-1 flex flex-col justify-center">
-                <div class="flex justify-between items-start mb-2">
+                <div class="flex justify-between items-start mb-2 gap-3">
                   <h3 class="text-xl font-bold text-white">
                     {{ item.event?.title || 'Evento non disponibile' }}
                   </h3>
